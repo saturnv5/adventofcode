0d46fa979044820a8508f683583bfb3d57a201ef
dxw <xiao.wang.saturnv@gmail.com>
2022-01-04 11:16:51 +1100

    Fixed bug in Direction enum.

--- src/com/dixie/adventofcode/lib/Direction.java
+++ src/com/dixie/adventofcode/lib/Direction.java
@@ -2,9 +2,9 @@ package com.dixie.adventofcode.lib;
 
 public enum Direction {
   NORTH(0, -1),
+  EAST(1, 0),
   SOUTH(0, 1),
-  WEST(-1, 0),
-  EAST(1, 0);
+  WEST(-1, 0);
 
   public final int dx, dy;
 
