8b1fa6f4a6a8dad5a5b939453f45d0922e68b43f
dxw <xiao.wang.saturnv@gmail.com>
2022-01-05 12:04:35 +1100

    DF reduction util in preparation for Day 14.

--- src/com/dixie/adventofcode/lib/GraphUtils.java
+++ src/com/dixie/adventofcode/lib/GraphUtils.java
@@ -3,6 +3,7 @@ package com.dixie.adventofcode.lib;
 import com.google.common.graph.*;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
 public class GraphUtils {
@@ -15,13 +16,11 @@ public class GraphUtils {
   }
 
   public static <N> Path<N> shortestPath(ValueGraph<N, Long> graph, N origin, N destination) {
-    return shortestPath(n -> graph.successors(n)
-            .stream()
-            .map(m -> Pair.of(m, graph.edgeValue(n, m).get()))
-            .toList(), origin, destination);
+    return shortestPath(successorFromValueGraph(graph), origin, destination);
   }
 
-  public static <N> Path<N> shortestPath(Function<N, Iterable<Pair<N, Long>>> successors, N origin, N destination) {
+  public static <N> Path<N> shortestPath(
+      Function<N, Iterable<Pair<N, Long>>> successors, N origin, N destination) {
     PriorityQueue<SearchNode<N>> fringe = new PriorityQueue<>();
     HashSet<N> visited = new HashSet<>();
     fringe.offer(new SearchNode<>(origin, null, 0));
@@ -41,6 +40,31 @@ public class GraphUtils {
       }
     }
     return null; // Not found.
+  }
+
+  public static <N, E> E depthFirstReduction(
+      ValueGraph<N, E> tree, E identity, BinaryOperator<E> accumulator, N origin) {
+    return depthFirstReduction(successorFromValueGraph(tree), identity, accumulator, origin);
+  }
+
+  public static <N, E> E depthFirstReduction(Function<N, Iterable<Pair<N, E>>> successors,
+      E identity, BinaryOperator<E> accumulator, N origin) {
+    E result = identity;
+    for (Pair<N, E> next : successors.apply(origin)) {
+      E subResult =
+          depthFirstReduction(successors, accumulator.apply(identity, next.second), accumulator,
+              next.first);
+      result = accumulator.apply(result, subResult);
+    }
+    return result;
+  }
+
+  public static <N, E> Function<N, Iterable<Pair<N, E>>> successorFromValueGraph(
+      ValueGraph<N, E> graph) {
+    return n -> graph.successors(n)
+        .stream()
+        .map(m -> Pair.of(m, graph.edgeValue(n, m).get()))
+        .toList();
   }
 
   private static class SearchNode<N> implements Comparable<SearchNode<N>> {
