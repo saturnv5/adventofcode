933d9dd00db23024351dc20d0760925ec4963d1f
dxw <xiao.wang.saturnv@gmail.com>
2022-01-06 15:28:11 +1100

    Day 16 part 2 brute force, too slow.

--- src/com/dixie/adventofcode/aoc2019/Day16.java
+++ src/com/dixie/adventofcode/aoc2019/Day16.java
@@ -5,6 +5,7 @@ import com.dixie.adventofcode.lib.Day;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Day16 extends Day {
   public static void main(String[] args) {
@@ -17,14 +18,35 @@ public class Day16 extends Day {
   protected long part1(List<String> lines) {
     int[] input =
         IntStream.range(0, lines.get(0).length()).map(i -> lines.get(0).charAt(i) - '0').toArray();
+    int[] output = calculateOutput(input);
+    StringBuilder sb = new StringBuilder();
+    while (sb.length() < 8) {
+      sb.append((char) (output[sb.length()] + '0'));
+    }
+    return Long.parseLong(sb.toString());
+  }
+
+  @Override
+  protected long part2(List<String> lines) {
+    String line = lines.get(0);
+    int targetPos = Integer.parseInt(line.substring(0, 8));
+    int[] input =
+        IntStream.range(0, lines.get(0).length()).map(i -> lines.get(0).charAt(i) - '0').toArray();
+    int[] repeatedInput =
+        Stream.generate(() -> input).limit(10000).flatMapToInt(Arrays::stream).toArray();
+    int[] output = calculateOutput(repeatedInput);
+    StringBuilder sb = new StringBuilder();
+    while (sb.length() < 8) {
+      sb.append((char) (output[targetPos + sb.length()] + '0'));
+    }
+    return Long.parseLong(sb.toString());
+  }
+
+  private static int[] calculateOutput(int[] input) {
     for (int i = 0; i < 100; i++) {
       input = executePhase(input);
     }
-    StringBuilder sb = new StringBuilder();
-    while (sb.length() < 8) {
-      sb.append((char) (input[sb.length()] + '0'));
-    }
-    return Long.parseLong(sb.toString());
+    return input;
   }
 
   private static int[] executePhase(int[] input) {
