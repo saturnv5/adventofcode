c83faadbc1a6c044b9b1ad116bd95dab821615b8
David X. Wang <xiao.wang.saturnv@gmail.com>
2022-01-09 10:18:39 +1100

    Day 19 part 2.

--- src/com/dixie/adventofcode/aoc2019/Day19.java
+++ src/com/dixie/adventofcode/aoc2019/Day19.java
@@ -13,14 +13,41 @@ public class Day19 extends Day {
     new Day19().solve();
   }
 
+  private long[] program;
+
+  @Override
+  protected long solve(List<String> lines, boolean part1) {
+    program = StreamUtils.streamLongs(lines.get(0), ",").toArray();
+    return super.solve(lines, part1);
+  }
+
   @Override
   protected long part1(List<String> lines) {
-    long[] program = StreamUtils.streamLongs(lines.get(0), ",").toArray();
-    Space2D<Boolean> space = new Space2D<>();
-    IntStream.range(0, 50)
-        .forEach(y -> IntStream.range(0, 50)
-            .forEach(x -> space.setValueAt(x, y, new Intcode(program).executeUntilEnd(x, y) == 1)));
-    System.out.println(space.toPrintableImage(b -> b ? "█" : " "));
-    return space.streamOccurrencesOf(true).count();
+    return IntStream.range(0, 50)
+        .mapToLong(y -> IntStream.range(0, 50).filter(x -> withinBeam(x, y)).count())
+        .sum();
+  }
+
+  @Override
+  protected long part2(List<String> lines) {
+    int x = 300, y = 200; // Can't imagine any input would have a beam large enough before 200.
+    // Zigzag down the right edge of the beam while checking the extremities of a 100x100 box.
+    // This only works by assuming the beam is solid and moves monotonically down and to the right.
+    while (true) {
+      if (withinBeam(x, y)) {
+        // Test the opposite corner.
+        if (withinBeam(x - 99, y + 99)) {
+          return y + (x - 99) * 10000;
+        } else {
+          x++; // Too low, zig rightwards.
+        }
+      } else {
+        y++; // Too high, zag downwards.
+      }
+    }
+  }
+
+  private boolean withinBeam(int x, int y) {
+    return new Intcode(program).executeUntilEnd(x, y) == 1;
   }
 }
