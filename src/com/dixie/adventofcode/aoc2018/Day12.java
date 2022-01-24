25562f02303cf32f69eafeb8ea0d1df429cad369
dxw <xiao.wang.saturnv@gmail.com>
2022-01-24 12:16:44 +1100

    Day 12 part 1 attempt. Incorrect.

--- src/com/dixie/adventofcode/aoc2018/Day12.java
+++ src/com/dixie/adventofcode/aoc2018/Day12.java
@@ -4,6 +4,7 @@ import com.dixie.adventofcode.lib.Day;
 import com.dixie.adventofcode.lib.Pair;
 import com.dixie.adventofcode.lib.Space2D;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,18 +15,34 @@ public class Day12 extends Day {
     new Day12().solve();
   }
 
+  private Space2D<Boolean> state;
+  private Map<List<Boolean>, Boolean> notes;
+
   @Override
-  protected Object part1(List<String> lines) {
-    Space2D<Boolean> state =
-        Space2D.parseFromStrings(List.of(lines.get(0).substring(15)), ch -> ch == '#');
-    Map<List<Boolean>, Boolean> notes = lines.subList(2, lines.size())
+  protected void prepare(List<String> lines) {
+    state = Space2D.parseFromStrings(List.of(lines.get(0).substring(15)), ch -> ch == '#');
+    notes = lines.subList(2, lines.size())
         .stream()
         .map(Day12::parseNote)
         .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond));
-    IntStream.range(0, 20).forEach(i -> {
+  }
 
-    });
+  @Override
+  protected Object part1(List<String> lines) {
+    IntStream.range(0, 20).forEach(i -> spread());
     return state.streamAllPoints().filter(state::getValueAt).mapToInt(p -> p.x).sum();
+  }
+
+  private void spread() {
+    Rectangle bounds = state.getBounds();
+    IntStream.range(bounds.x - 2, bounds.x + bounds.width + 3).forEach(i -> {
+      List<Boolean> surrounds =
+          IntStream.range(i - 2, i + 3).mapToObj(x -> state.getValueAt(x, 0, false)).toList();
+      boolean newValue = notes.getOrDefault(surrounds, false);
+      if (newValue != state.getValueAt(i, 0, false)) {
+        state.setValueAt(i, 0, notes.getOrDefault(surrounds, false));
+      }
+    });
   }
 
   private static Pair<List<Boolean>, Boolean> parseNote(String note) {
