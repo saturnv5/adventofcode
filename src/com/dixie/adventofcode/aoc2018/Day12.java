c585d5c3024ab96c49964a7e0128c88f23bf40b1
dxw <xiao.wang.saturnv@gmail.com>
2022-01-24 15:58:18 +1100

    Day 12 part 2.

--- src/com/dixie/adventofcode/aoc2018/Day12.java
+++ src/com/dixie/adventofcode/aoc2018/Day12.java
@@ -15,12 +15,13 @@ public class Day12 extends Day {
     new Day12().solve();
   }
 
+  private static final long FIFTY_BILLION = 50_000_000_000L;
+
   private Space2D<Boolean> state;
   private Map<List<Boolean>, Boolean> notes;
 
   @Override
   protected void prepare(List<String> lines) {
-    state = Space2D.parseFromStrings(List.of(lines.get(0).substring(15)), ch -> ch == '#');
     notes = lines.subList(2, lines.size())
         .stream()
         .map(Day12::parseNote)
@@ -28,9 +29,37 @@ public class Day12 extends Day {
   }
 
   @Override
+  protected Object solve(List<String> lines, boolean part1) {
+    state = Space2D.parseFromStrings(List.of(lines.get(0).substring(15)), ch -> ch == '#');
+    return super.solve(lines, part1);
+  }
+
+  @Override
   protected Object part1(List<String> lines) {
     IntStream.range(0, 20).forEach(i -> spread());
-    return state.streamAllPoints().filter(state::getValueAt).mapToInt(p -> p.x).sum();
+    return currentSum();
+  }
+
+  @Override
+  protected Object part2(List<String> lines) {
+    long steps = 0;
+    int lastSum = currentSum();
+    int lastDelta = 0;
+    int sameDeltas = 0;
+    while (steps < FIFTY_BILLION && sameDeltas < 10) {
+      // If we get same delta 10 times, that's good enough to project forwards to 50 billion.
+      spread();
+      steps++;
+      int delta = currentSum() - lastSum;
+      if (lastDelta == delta) {
+        sameDeltas++;
+      } else {
+        lastDelta = delta;
+        sameDeltas = 0;
+      }
+      lastSum += delta;
+    }
+    return lastSum + lastDelta * (FIFTY_BILLION - steps);
   }
 
   private void spread() {
@@ -45,6 +74,10 @@ public class Day12 extends Day {
       }
     });
     state = newState;
+  }
+
+  private int currentSum() {
+    return state.streamAllPoints().filter(state::getValueAt).mapToInt(p -> p.x).sum();
   }
 
   private static Pair<List<Boolean>, Boolean> parseNote(String note) {
