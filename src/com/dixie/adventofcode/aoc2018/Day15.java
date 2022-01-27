336384059728029c5a3b09a572d5aef2511f98be
David X. Wang <xiao.wang.saturnv@gmail.com>
2022-01-27 21:23:14 +1100

    Day 15 progress.

--- src/com/dixie/adventofcode/aoc2018/Day15.java
+++ src/com/dixie/adventofcode/aoc2018/Day15.java
@@ -6,7 +6,9 @@ import com.dixie.adventofcode.lib.Space2D;
 
 import java.awt.*;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 public class Day15 extends Day {
@@ -14,16 +16,24 @@ public class Day15 extends Day {
     new Day15().solve();
   }
 
-  private static final Comparator<Point> READING_ORDER =
-      Comparator.<Point>comparingInt(p -> p.y).thenComparingInt(p -> p.x);
   private static final List<Direction> SUCCESSOR_DIRECTIONS =
       List.of(Direction.NORTH, Direction.WEST, Direction.EAST, Direction.SOUTH);
 
   private Space2D<Unit> space;
+  private int numElves;
+  private int numGoblins;
 
   @Override
   protected Object solve(List<String> lines, boolean part1) {
     space = Space2D.parseFromStrings(lines, Day15::parseCell);
+    numElves = (int) space.streamAllPoints()
+        .map(space::getValueAt)
+        .filter(u -> u != null && u.type == Unit.Type.ELF)
+        .count();
+    numGoblins = (int) space.streamAllPoints()
+        .map(space::getValueAt)
+        .filter(u -> u != null && u.type == Unit.Type.GOBLIN)
+        .count();
     return super.solve(lines, part1);
   }
 
@@ -36,8 +46,25 @@ public class Day15 extends Day {
     };
   }
 
-  private void simulateRound() {
+  private boolean simulateRound() {
+    HashSet<Unit> hasMoved = new HashSet<>(numElves + numGoblins);
+    AtomicBoolean hasCombatEnded = new AtomicBoolean(false);
+    space.streamAllPointsInBounds().forEach(p -> {
+      if (hasCombatEnded.get()) return;
+      Unit unit = space.getValueAt(p);
+      if (unit == null || unit.type == Unit.Type.WALL) {
+        return;
+      }
+      if (!hasMoved.add(unit)) {
+        return;
+      }
+      hasCombatEnded.set(simulateUnit(p, unit));
+    });
+    return hasCombatEnded.get();
+  }
 
+  private boolean simulateUnit(Point location, Unit unit) {
+    return false;
   }
 
   private Stream<Point> successors(Point from) {
