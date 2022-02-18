3a4d1ec678cbb44c823733269916346c933f89a1
David X. Wang <xiao.wang.saturnv@gmail.com>
2022-02-18 13:26:52 +1100

    Day 19 translated from machine code to Java.

--- src/com/dixie/adventofcode/aoc2018/Day19.java
+++ src/com/dixie/adventofcode/aoc2018/Day19.java
@@ -19,6 +19,58 @@ public class Day19 extends Day {
       program.get(r[ipReg]).execute(r);
       r[ipReg]++;
     }
-    return r[0];
+    execute();
+    return r0;
+  }
+
+  int r0, r1, r2, r4, r5;
+
+  private void execute() {
+    // Move forward 16 + 1 instructions.
+    r4 += 2;
+    r4 *= r4;
+    r4 *= 19;
+    r4 *= 11;
+    r1 += 4;
+    r1 *= 22;
+    r1 += 2;
+    r4 += r1;
+    if (r0 == 0) {
+      // Back to start.
+      executeSub();
+      return;
+    }
+    r1 = 27;
+    r1 *= 28;
+    r1 += 29;
+    r1 *= 30;
+    r1 += 14;
+    r1 *= 32;
+    r4 += r1;
+    r0 = 0;
+    executeSub();
+  }
+
+  private void executeSub() {
+    r2 = 1;
+    r5 = 1;
+    while (true) {
+      r1 = r2 * r5;
+      r1 = r1 == r4 ? 1 : 0;
+      if (r1 == 1) {
+        r0 += r2;
+      }
+      r5++;
+      r1 = r5 > r4 ? 1 : 0;
+      if (r1 == 0) {
+        continue;
+      }
+      r2++;
+      r1 = r2 > r4 ? 1 : 0;
+      if (r1 == 1) {
+        return;
+      }
+      r5 = 1;
+    }
   }
 }
