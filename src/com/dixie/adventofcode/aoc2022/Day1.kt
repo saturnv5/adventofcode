6532351a388b122943b82b09e35b26c2aa40ada4
dxw <xiao.wang.saturnv@gmail.com>
2022-12-01 11:13:34 +1100

    AoC 2022 Day 1 prep.

---
+++ src/com/dixie/adventofcode/aoc2022/Day1.kt
@@ -0,0 +1,13 @@
+package com.dixie.adventofcode.aoc2022
+
+import com.dixie.adventofcode.klib.Day
+
+class Day1 : Day() {
+  override fun part1(): String {
+    return super.part1()
+  }
+}
+
+fun main() {
+  Day1().run()
+}
