ee861c2c641eccd0e9ab40b6fef2ed15be0eec28
dxw <xiao.wang.saturnv@gmail.com>
2022-12-06 17:17:45 +1100

    AoC 2022 Day 6 completed.

--- src/com/dixie/adventofcode/aoc2022/Day6.kt
+++ src/com/dixie/adventofcode/aoc2022/Day6.kt
@@ -2,7 +2,25 @@ package com.dixie.adventofcode.aoc2022
 
 import com.dixie.adventofcode.klib.Day
 
-class Day6 : Day() {}
+class Day6 : Day() {
+  override fun part1(): Any {
+    val msg = lines[0]
+    for (i in 4..msg.length) {
+      val set = setOf(msg[i - 4], msg[i - 3], msg[i - 2], msg[i - 1])
+      if (set.size == 4) return i
+    }
+    return "Start-of-packet marker not found!"
+  }
+
+  override fun part2(): Any {
+    val msg = lines[0]
+    for (i in 14..msg.length) {
+      val set = (1..14).map { msg[i - it] }.toSet()
+      if (set.size == 14) return i
+    }
+    return "Message marker not found!"
+  }
+}
 
 fun main() {
   Day6().run()
